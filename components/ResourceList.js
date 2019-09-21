96cb43995e8c1df6333bd0ec28a63658cea1bbeb
chengzhang <676931854@qq.com>
2019-09-22 01:08:18 +0800

    update

--- components/ResourceList.js
+++ components/ResourceList.js
@@ -11,15 +11,30 @@ import store from 'store-js';
 import { Redirect } from '@shopify/app-bridge/actions';
 import { Context } from '@shopify/app-bridge-react';
 
-const GET_ORDERS_BY_ID = gql`
-  query getO($ids: [ID!]!) {
+const GET_PRODUCTS_BY_ID = gql`
+  query getProducts($ids: [ID!]!) {
     nodes(ids: $ids) {
-      ... on Order {
-        name
-        email
+      ... on Product {
+        title
+        handle
+        descriptionHtml
         id
-       
-        
+        images(first: 1) {
+          edges {
+            node {
+              originalSrc
+              altText
+            }
+          }
+        }
+        variants(first: 1) {
+          edges {
+            node {
+              price
+              id
+            }
+          }
+        }
       }
     }
   }
@@ -30,7 +45,7 @@ class ResourceListWithProducts extends React.Component {
 
   render() {
     const app = this.context;
-    const redirectToOrder = () => {
+    const redirectToProduct = () => {
       const redirect = Redirect.create(app);
       redirect.dispatch(
         Redirect.Action.APP,
@@ -40,23 +55,37 @@ class ResourceListWithProducts extends React.Component {
 
     const twoWeeksFromNow = new Date(Date.now() + 12096e5).toDateString();
     return (
-      <Query query={GET_ORDERS_BY_ID} variables={{ ids: store.get('ids') }}>
+      <Query query={GET_PRODUCTS_BY_ID} variables={{ ids: store.get('ids') }}>
         {({ data, loading, error }) => {
           if (loading) { return <div>Loading…</div>; }
           if (error) { return <div>{error.message}</div>; }
-          console.log("hehe");
           console.log(data);
-          console.log("hehe2");
           return (
             <Card>
               <ResourceList
                 showHeader
-                resourceName={{ singular: 'Order', plural: 'Orders' }}
+                resourceName={{ singular: 'Product', plural: 'Products' }}
                 items={data.nodes}
                 renderItem={(item) => {
+                  const media = (
+                    <Thumbnail
+                      source={
+                        item.images.edges[0]
+                          ? item.images.edges[0].node.originalSrc
+                          : ''
+                      }
+                      alt={
+                        item.images.edges[0]
+                          ? item.images.edges[0].node.altText
+                          : ''
+                      }
+                    />
+                  );
+                  const price = item.variants.edges[0].node.price;
                   return (
                     <ResourceList.Item
                       id={item.id}
+                      media={media}
                       accessibilityLabel={`View details for ${item.title}`}
                       onClick={() => {
                         store.set('item', item);
@@ -73,9 +102,11 @@ class ResourceListWithProducts extends React.Component {
                           </h3>
                         </Stack.Item>
                         <Stack.Item>
-                          <p>${name}</p>
+                          <p>${price}</p>
                         </Stack.Item>
-                       
+                        <Stack.Item>
+                          <p>Expires on {twoWeeksFromNow} </p>
+                        </Stack.Item>
                       </Stack>
                     </ResourceList.Item>
                   );
